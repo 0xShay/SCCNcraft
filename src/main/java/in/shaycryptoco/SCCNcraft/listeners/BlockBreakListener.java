4bb7a66bb710d370bfb0b293e3e75e16494e609e
ShayGaming24 <sgaming24@outlook.com>
2020-08-12 11:01:14 +0100

    Mine diamonds => SCCN

--- src/main/java/in/shaycryptoco/SCCNcraft/listeners/BlockBreakListener.java
+++ src/main/java/in/shaycryptoco/SCCNcraft/listeners/BlockBreakListener.java
@@ -1,13 +1,11 @@
 package in.shaycryptoco.SCCNcraft.listeners;
 
-import in.shaycryptoco.SCCNcraft.Main;
 import in.shaycryptoco.SCCNcraft.events.SpawnerBreakEvent;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,6 +16,8 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Random;
 import java.util.logging.Logger;
 
+import static in.shaycryptoco.SCCNcraft.Main.*;
+
 public class BlockBreakListener implements Listener {
 
     private static final Logger log = Logger.getLogger("Minecraft");
@@ -25,26 +25,27 @@ public class BlockBreakListener implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
 
-        Economy eco = Main.getEconomy();
+        Economy eco = getEconomy();
         Player p = e.getPlayer();
 
-//        if (e.getBlock().getType().equals(Material.DIAMOND_ORE)) {
-//
-//            ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
-//            if (!item.getEnchantments().containsKey(Enchantment.SILK_TOUCH)) {
-//
-//                // reward for mining 1 diamond ore
-//                Random rand = new Random();
-//                double mining_reward = Double.parseDouble(String.format("%.2f", (rand.nextDouble() * 4) + 1));
-//
-//                EconomyResponse response = eco.depositPlayer(p, mining_reward);
-//                if (response.transactionSuccess()) {
-//                    p.sendMessage(ChatColor.GREEN + "You got " + ChatColor.AQUA + mining_reward + ChatColor.GREEN + " SCCN for mining a diamond ore!");
-//                }
-//
-//            }
-//
-//        }
+        if (e.getBlock().getType().equals(Material.DIAMOND_ORE)) {
+
+            ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
+            if (!item.getEnchantments().containsKey(Enchantment.SILK_TOUCH)) {
+
+                // reward for mining 1 diamond ore
+                Random rand = new Random();
+                double mining_reward = Double.parseDouble(String.format("%.2f", (rand.nextDouble() * 2) + 1));
+                // random from 1 to 3
+
+                EconomyResponse response = eco.depositPlayer(p, mining_reward);
+                if (response.transactionSuccess()) {
+                    p.sendMessage(ChatColor.GREEN + "You got " + ChatColor.AQUA + mining_reward + ChatColor.GREEN + " SCCN for mining a diamond ore!");
+                }
+
+            }
+
+        }
 
         if (e.getPlayer().hasPermission("sccncraft.general.silkspawners") && e.getBlock().getType().equals(Material.SPAWNER)) {
 
